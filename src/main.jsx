650c238588ef8d2f0ae18f99705870818983361a
umakant katare <umakantkatare11@gmail.com>
2026-01-25 21:06:39 +0530

    resolve error

--- src/main.jsx
+++ src/main.jsx
@@ -13,17 +13,9 @@ import SignUp from "./pages/SignUp.jsx";
 import AuthContextWrapper from "./context/AuthContext.jsx";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
-<<<<<<< HEAD
-<<<<<<< Updated upstream
 import SearchResult from "./components/searchResult.jsx";
-=======
-import SearchResult from "./components/SearchResult.jsx";
->>>>>>> 46e9a38c5a8f3ba7e7d34a1e246e91f67d2aa0b6
 import CategoryProduct from './pages/CategoryProduct';
-=======
-import SearchResult from "./components/SearchResult.jsx";
 import CategoryProduct from "./pages/CategoryProduct";
->>>>>>> Stashed changes
 const router = createBrowserRouter([
   {
     path: "/",
